293c7daefa7636bf0f4026dc7e7bc59f61db5cc6
bhavna <bhavna25sinha@gmail.com>
2018-09-29 22:39:59 -0400

    Part 3 - First Commit

--- part3/assign.py
+++ part3/assign.py
@@ -1,14 +1,64 @@
+#(1) a description of how you formulated the search problem, including precisely dening the state space, the successor function,
+#the edge weights, the goal state, and (if applicable) the heuristic function(s) you designed, including an
+#argument for why they are admissible;
+
+#Answer: So the problem statement for this quetsion is to to reduce the K,M,N time which is reduce the no of time to correct
+#assignment which depends on the number of teams which is the K factor, Secondly reducing the N factor that is grouping them
+#with the desired team number and member and thirdly M which is not grouping them with the non [refred member.
+#Initial state: I assumed my initial state to be member with highest time
+#Goal state: to reduce time of each highest member to the lowest by groupig it with other memebers in group of two/ three.
+# If grouping reduces its individual cost, else leave it alone.
+#sucessor function : individual with their costs, all possible combinations with rest of members in group of two or three and
+# their time. #the edge weight: total time taken by considering factors like m, n and 1 min(meeting with instructor)  involved.
+
+#(2) a brief description of how your search algorithm works;
+# 1) Taking my input file and breaking each line and storing it in the list
+#2) Secondly I am taking a dictionary for group size, prefer member and non prefermeber
+#3)Now slpitting everuthing for example I am storing my user 1 along with theor prefred memebr in dictionar
+#4) secondly i am splitting an storing the group prefrence of each user
+#5) thirdly i am storing the non prefred memeber they have listed. In all, I am dividing each and every part of the file which
+#was entered into different dictionary so that it can be accessable easily so now i have seperate information for all user
+#user with prefer group number, user with prefer number, user with non prefer memeber
+#6)Firstly my team is grouped single, I am conidering every member to be team alone and I am calculating the cost for each team
+#i.e single team so now each user has a number associated with them which is the cost. I am using cost_function() to calculate
+#the cost for the team.
+#7 I am taking a priority queue to sort my list so the member with highest cost will come out first and then I can group that memebr
+#with other member and check their cost if that cost is less than the initial cost or not
+#8 so now my one meber comes out the priority queue, i will group that member with remianing other member and calculate that cost
+#while looking at the other values provided such as prefred member, no prefer memebr and group size
+#9 now after calculating the cost for all i will pick this group of 2(A,B suppose) who have the least cost and push them in the
+#list of my team so now my team has suppose {AB, C, D, E, F}. Now each have their cost associated with it . Now I will again
+#pick up the team with highest cost and try to minimize the cost using the same procedure suppose among all AB have the highest
+# cost , Now I will again try to group that with other member so now {ABC,D,E,F} so now I will have the cost so once my team is
+#made of three and the cost is less as compared to the previous cost , I will pust that out and proceed with other members and
+#group them
+#10 So at the end I will have my team grouped with other memeber, and with the cost which is less than the initial cost.
+#11 In case 'test' size = 2 or 1(it will happen if number of member size is not divisible by 3), it groups them togther and
+# calculates its time and prints them.
+
+#(3) any discussion of any problems you faced, any assumptions, simplications, and/or design decisions you made.
+#Answer: Firstly I tried the combination thing of generating all the possible states, such as groouping A with B, C,D,E,F
+#and then taking the group with lowest number and trying to minimize again with grouping it with ABC,ABD,ABE, ABF , but in case
+#of small data it works properly but with large number , the states generation was high and redundancy was there, so I modified
+#algorithm as above to optimize and return the result in minimum time possible.
+
+
 import os
 import sys
 try:
     import Queue as Q  # ver. < 3.0
 except ImportError:
     import queue as Q
-k = 4
-m = 5
-n = 2
+# k = 4
+# m = 5
+# n = 2
 
-input_file = "E:/IUB/AI/Assignments/Assignment 2/rajthakk-nabiyuni-bhsinha-a1/part3/assign.txt"
+input_file = sys.argv[1]
+k = int(sys.argv[2])
+m = int(sys.argv[3])
+n = int(sys.argv[4])
+
+#input_file = "E:/IUB/AI/Assignments/Assignment 2/rajthakk-nabiyuni-bhsinha-a1/part3/assign.txt"
 #reading from the file
 file = open(input_file , 'r')
 input_list = []
